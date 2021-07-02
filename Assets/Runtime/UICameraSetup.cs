ae0f4354c8f85f2e85244627493abe285892dd6d
tangwilliam <tangqinwei@gmail.com>
2021-07-02 21:02:33 +0800

    为UI相机设置StoreAction

---
+++ Assets/Runtime/UICameraSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TA
+{
+    public class UICameraSetup : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Camera camera = GetComponent<Camera>();
+            if (camera == null) return;
+
+            CameraSetup.LoadStoreActionSetup(camera, Color.black);
+        }
+
+    }
+}
+
+
